1a1dd4cfebffe45f7e3374aafa3c16b2a180771e
fsande <fsande@ull.es>
2020-04-24 17:52:57 +0100

    Events examples

---
+++ src/T4-Events/15-2event-listeners.js
@@ -0,0 +1,35 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author F. de Sande
+ * @since 24.apr.2020
+ * @desc JS Events. 2 Events Listeners
+ */
+
+"use strict"
+
+function onOuterClick() {
+  outer.classList.add('selected');
+  console.log('Outer clicked!');
+}
+
+function onInnerClick() {
+  inner.classList.add('selected');
+  console.log('Inner clicked!');
+}
+
+function onResetClick() {
+  inner.classList.remove('selected');
+  outer.classList.remove('selected');
+}
+
+const outer = document.querySelector('#outer');
+const inner = document.querySelector('#inner');
+outer.addEventListener('click', onOuterClick);
+inner.addEventListener('click', onInnerClick);
+
+const reset = document.querySelector('button');
+reset.addEventListener('click', onResetClick);
