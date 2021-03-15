27161b93fa92b61323806d658493b6feac4f12c1
fsande <fsande@ull.es>
2021-03-15 17:06:47 +0000

    emulating class behaviour example

---
+++ src/T1F-OOP/emulating-class-behavior.js
@@ -0,0 +1,50 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author F. de Sande
+ * @since 15.Mar.2021
+ * @desc Emulating class Behavior with prototypes
+ *       At Line A, we have to set FatFreeFood.prototype equal to a new object, 
+ *       whose prototype reference is to Food.prototype. 
+ *       If we fail to do this, our "child classes" won't have access to "superclass" methods.
+ *
+ *       Unfortunately, this results in the rather bizarre behavior that FatFreeFood.constructor 
+ *       is Function... Not FatFreeFood. 
+ *       So, to keep everything sane, we have to manually set FatFreeFood.constructor by hand at Line B.
+ * {@link https://scotch.io/tutorials/better-javascript-with-es6-pt-ii-a-deep-dive-into-classes}
+ */
+
+'use strict';
+
+function Food(name, protein, carbs, fat) {
+  this.name = name;
+  this.protein = protein;
+  this.carbs = carbs;
+  this.fat = fat;
+}
+
+Food.prototype.toString = function () {
+  return `${this.name} | ${this.protein}g P :: ${this.carbs}g C :: ${this.fat}g F`;
+};
+
+function FatFreeFood (name, protein, carbs) {
+  Food.call(this, name, protein, carbs, 0);
+}
+
+// Setting up "subclass" relationships
+// =====================
+// LINE A :: Using Object.create to manually set FatFreeFood's "parent".
+FatFreeFood.prototype = Object.create(Food.prototype);
+
+// LINE B :: Manually (re)setting constructor reference (!)
+Object.defineProperty(FatFreeFood.constructor, "constructor", {
+  enumerable : false,
+  writeable      : true,
+  value             : FatFreeFood
+});
+
+let healthyFood = new FatFreeFood('vegetables', 0, 0, 0);
+console.log(healthyFood.toString());
