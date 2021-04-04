3d468fb8887419f7016de4383fd53ef10e950c72
fsande <fsande@ull.es>
2021-04-04 18:50:30 +0100

    Minor Fixes

--- src/T2-GraphicsProgramming/simple-example.js
+++ src/T2-GraphicsProgramming/simple-example.js
@@ -1,13 +1,13 @@
-function draw() {
+const draw = function() {
   const CANVAS = document.getElementById('tutorial');
   if (CANVAS.getContext) {
-    const CTX = CANVAS.getContext('2d');
+    let ctx = CANVAS.getContext('2d');
 
 		// outlined square X: 50, Y: 35, width/height 50
-    CTX.strokeRect(50, 35, 50, 50);
+    ctx.strokeRect(50, 35, 50, 50);
 
     // filled square X: 125, Y: 35, width/height 50
-    CTX.fillStyle = 'rgba(0, 0, 200, 0.5)';
-    CTX.fillRect(125, 35, 50, 50);
+    ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+    ctx.fillRect(125, 35, 50, 50);
   }
 }
