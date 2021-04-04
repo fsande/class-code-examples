e40f6cfc324e912dd050003138f31ebbb426c107
fsande <fsande@ull.es>
2021-04-04 19:10:19 +0100

    Enhance rectangle class

--- src/T2-GraphicsProgramming/canvasFace.js
+++ src/T2-GraphicsProgramming/canvasFace.js
@@ -1,16 +1,16 @@
-function draw() {
+const draw = function() {
   const CANVAS = document.getElementById('tutorial');
   if (CANVAS.getContext) {
-    const CTX = CANVAS.getContext('2d');
+    let ctx = CANVAS.getContext('2d');
 
-    CTX.beginPath();
-    CTX.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
-    CTX.moveTo(110, 75);
-    CTX.arc(75, 75, 35, 0, Math.PI, false);  // Mouth (clockwise)
-    CTX.moveTo(65, 65);
-    CTX.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
-    CTX.moveTo(95, 65);
-    CTX.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
-    CTX.stroke();
+    ctx.beginPath();
+    ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+    ctx.moveTo(110, 75);
+    ctx.arc(75, 75, 35, 0, Math.PI, false);  // Mouth (clockwise)
+    ctx.moveTo(65, 65);
+    ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Left eye
+    ctx.moveTo(95, 65);
+    ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Right eye
+    ctx.stroke();
   }
 }
--- src/T2-GraphicsProgramming/classRectangle.js
+++ src/T2-GraphicsProgramming/classRectangle.js
@@ -17,18 +17,28 @@
  * @desc setup canvas
  */
 const CANVAS = document.getElementById('tutorial');
-const CTX = CANVAS.getContext('2d');
+const ctx = CANVAS.getContext('2d');
 
 /**
- * @desc function to create rectangle objects
+ * @desc A class to represent rectangle objects
+ * @class
  */
 class Rectangle {
-  // you create new Rectangles by calling this as a function
-  // these are the arguments you pass in
-  // add default values to avoid errors on empty arguments
-  constructor (
-    x = 0, y = 0,
-    width = 0, height = 0,
+	/**
+   * Create a rectangle
+	 * these are the arguments you pass in
+   * add default values to avoid errors on empty arguments
+   * @constructor
+   * @public
+   * @param {number} x - X coord of the rectangle's position
+   * @param {number} y - Y coord of the rectangle's position
+   * @param {number} width  
+   * @param {number} height  
+   * @param {string} fillColor  
+   * @param {string} strokeColor  
+   * @param {number} strokeWidth  
+   */
+  constructor ( x = 0, y = 0, width = 0, height = 0,
     fillColor = '', strokeColor = '', strokeWidth = 2
   ) {
     // ensure the arguments passed in are numbers
@@ -46,30 +56,30 @@ class Rectangle {
    * @desc get keyword causes this method to be called when you use myRectangle.area
 	 * @returns Area of the rectangle
    */
-  get area () {
+  getArea () {
     return this.width * this.height
   }
 
   // gets the X position of the left side
-  get left () {
+  getLeft () {
     // origin is at top left so just return x
     return this.x
   }
 
   // get X position of right side
-  get right () {
+  getRight () {
     // x is left position + the width to get end point
     return this.x + this.width
   }
 
   // get the Y position of top side
-  get top () {
+  getTop () {
     // origin is at top left so just return y
     return this.y
   }
 
   // get Y position at bottom
-  get bottom () {
+  getBottom () {
     return this.y + this.height
   }
 
@@ -84,25 +94,25 @@ class Rectangle {
 
     // saves the current styles set elsewhere
     // to avoid overwriting them
-    CTX.save()
+    ctx.save()
   
     // set the styles for this shape
-    CTX.fillStyle = fillColor
-    CTX.lineWidth = strokeWidth
+    ctx.fillStyle = fillColor
+    ctx.lineWidth = strokeWidth
 
     // create the *path*
-    CTX.beginPath()
-    CTX.strokeStyle = strokeColor
-    CTX.rect(x, y, width, height)
+    ctx.beginPath()
+    ctx.strokeStyle = strokeColor
+    ctx.rect(x, y, width, height)
 
     // draw the path to screen
-    CTX.fill()
-    CTX.stroke()
+    ctx.fill()
+    ctx.stroke()
 
     // restores the styles from earlier
     // preventing the colors used here
     // from polluting other drawings
-    CTX.restore()
+    ctx.restore()
   }
 }
 
@@ -134,4 +144,3 @@ mySquare1.draw();
 mySquare2.draw();
 mySquare3.draw();
 mySquare4.draw();
-
