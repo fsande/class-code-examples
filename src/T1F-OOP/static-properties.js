16e163f3560270306751fef5e666fae0e3e0c9d6
fsande <fsande@ull.es>
2021-04-09 14:17:31 +0100

    static properties

---
+++ src/T1F-OOP/static-properties.js
@@ -0,0 +1,20 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author F. de Sande
+ * @since 09.Apr.2021
+ * @desc Static Properties
+ *       ES6 Classes support static members
+ *
+ */
+
+'use strict';
+
+class Circle {
+  static DEGREES_IN_CIRCLE = () => {
+    return 360;
+  }
+}
