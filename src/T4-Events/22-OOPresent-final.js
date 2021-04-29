7d0f423b13a0dd7216192d1b4925d07894e8030c
fsande <fsande@ull.es>
2021-04-29 16:06:47 +0100

    Code enhancement

--- src/T4-Events/22-OOPresent-final.js
+++ src/T4-Events/22-OOPresent-final.js
@@ -12,14 +12,14 @@
 'use strict';
 
 class Present {
-  #containerElement = undefined;
+  #containerElement = undefined; /** The The DOM element that will host the present image */
   constructor(containerElement) {
     this.#containerElement = containerElement;
 
     // Create image and append to container.
-    const image = document.createElement('img');
+    let image = document.createElement('img');
     image.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1083533/gift-icon.png';
-		image.addEventListener('click', this.#openPresent);
+		image.addEventListener('click', this.openPresent);
     this.#containerElement.append(image);
   }
 
@@ -28,9 +28,9 @@ class Present {
 	 * @method
    * @description Changes the DOM with the new image and removes the listener
    */
-  #openPresent(event) {
+  openPresent(event) {
   	const image = event.currentTarget;
     image.src = 'https://media.giphy.com/media/27ppQUOxe7KlG/giphy.gif'; // The new image
-  	image.removeEventListener('click', this._openPresent);
+  	image.removeEventListener('click', this.openPresent);
   }
 }
--- src/T4-Events/23-OOPresent-this.js
+++ src/T4-Events/23-OOPresent-this.js
@@ -12,18 +12,22 @@
 'use strict';
 
 class Present {
-  constructor(containerElement) {
-    this.containerElement = containerElement;
+  /** @private */
+  #containerElement = undefined;  /** The The DOM element that will host the Present */
+  #image = undefined;             /** The DOM element that will be created to host the image */
 
-    // Create image and append to container.
-    this.image = document.createElement('img');
-    this.image.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1083533/gift-icon.png';
-		this.image.addEventListener('click', this._openPresent);
-    this.containerElement.append(this.image);
+  constructor(containerElement) {
+    this.#containerElement = containerElement;
+
+    // Create image and append to container
+    this.#image = document.createElement('img');
+    this.#image.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/1083533/gift-icon.png';
+		this.#image.addEventListener('click', this.openPresent);
+    this.#containerElement.append(this.#image);
   }
 
-  _openPresent(event) {
-    this.image.src = 'https://media.giphy.com/media/27ppQUOxe7KlG/giphy.gif';
-  	this.image.removeEventListener('click', this._openPresent);
+  openPresent(event) {
+    this.#image.src = 'https://media.giphy.com/media/27ppQUOxe7KlG/giphy.gif';
+  	this.#image.removeEventListener('click', this.openPresent);
   }
 }
