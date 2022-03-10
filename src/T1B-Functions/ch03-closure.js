bbc519bdee429bf2e43c8113fb6a89049106d59f
fsande <fsande@ull.es>
2022-03-10 12:56:42 +0000

    code enhancement

--- src/T1B-Functions/ch03-closure.js
+++ src/T1B-Functions/ch03-closure.js
@@ -1,25 +1,30 @@
 /**
- * @author       F. de Sande
- * @since 4.dec.2019
- *
- * Book Eloquent Javascrip 
- * Chapter 03
- *
- * @fileOverview 
- * Illustrates Closure
- * The code defines a function, wrapValue, that creates a local binding. 
- * It then returns a function that accesses and returns this local binding.
- *
- * Thinking about programs like this takes some practice. 
- * A good mental model is to think of function values as containing both the code in their body 
- * and the environment in which they are created. 
- * When called, the function body sees the environment in which it was created, 
- * not the environment in which it is called.
- * 
- * En el segundo ejemplo, se llama multiplier y se crea un entorno en el que su parámetro factor está vinculado a 2. 
- * El valor de la función que devuelve, que se almacena en 'twice', recuerda este entorno. 
- * Así que cuando se llama así, multiplica su argumento por 2.
- */
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * Programación de Aplicaciones Interactivas
+  *
+  * @author       F. de Sande
+  * @since 4.dec.2019
+  *
+  * Book Eloquent Javascrip 
+  * Chapter 03
+  *
+  * @desc
+  * Illustrates Closure
+  * The code defines a function, wrapValue, that creates a local binding. 
+  * It then returns a function that accesses and returns this local binding.
+  *
+  * Thinking about programs like this takes some practice. 
+  * A good mental model is to think of function values as containing both the code in their body 
+  * and the environment in which they are created. 
+  * When called, the function body sees the environment in which it was created, 
+  * not the environment in which it is called.
+  * 
+  * En el segundo ejemplo, se llama multiplier y se crea un entorno en el que su parámetro factor está vinculado a 2. 
+  * El valor de la función que devuelve, que se almacena en 'twice', recuerda este entorno. 
+  * Así que cuando se llama así, multiplica su argumento por 2.
+**/
 
 'use strict';
 
--- src/T1B-Functions/closure.js
+++ src/T1B-Functions/closure.js
@@ -9,18 +9,22 @@
   * @desc Closure example
   * 
   * Functions retain access to their original scope, even when the outer function they were defined in has returned
+  * Experiment calling directly to gimmeFunction() instead of calling it through myFunction();
   *
   */
 
 'use strict';
 
+// @returns {function} Returns a function which prints the localVar
 let gimmeFunction = function() {
-  let myVar = "I'm in here!";
+  let localVar = "I'm in here!";
 
   return function() {
-    console.log(myVar);
+    console.log(localVar);
   }
 }
 
-let myFunction = gimmeFunction();
+let myFunction = gimmeFunction(); // assigns to myFunction the function returned by gimmeFunction()
 myFunction();
+
+// gimmeFunction();
