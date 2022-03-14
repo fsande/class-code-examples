8808c151f722f1ea894673e08427a650e1e0ddad
fsande <fsande@ull.es>
2022-03-14 18:14:54 +0000

    main() added

--- src/T1C-Objects-Strings-Arrays/objects.js
+++ src/T1C-Objects-Strings-Arrays/objects.js
@@ -12,64 +12,67 @@
 
 'use strict';
 
-let course = {
+function main() {
+  let course = {
     name:'Programación de Aplicaciones Interactivas',
     section:3,
     undergraduate:true
-};
+  };
 
-console.log(course.section);      // → 2
-console.log(course["section"]);    // → 2
+  console.log(course.section);      // → 2
+  console.log(course["section"]);    // → 2
 
-let exclamation = 'wow!';
-let listOfExclamations = ['golly!', 'gosh!'];
-console.log(exclamation.length, listOfExclamations.length)
+  let exclamation = 'wow!';
+  let listOfExclamations = ['golly!', 'gosh!'];
+  console.log(exclamation.length, listOfExclamations.length)
 
-let obj = {};
-function f() {
-  console.log("Hi, I'm a method!");
-}
-obj.doStuff = f;
-
-
-let obj2 = {
-  doStuff: function() {
+  let obj = {};
+  function f() {
     console.log("Hi, I'm a method!");
-  },
-};
+  }
+  obj.doStuff = f;
 
 
-let obj3 = {};
-obj.doStuff = function() {
-  console.log("Hi, I'm a method!");
-};
+  let obj2 = {
+    doStuff: function() {
+      console.log("Hi, I'm a method!");
+    }
+  };
 
-console.log('Nothing to see: ', course.nothingToSeeHere);
 
-course.nothingToSeeHere = 'maybe something';
-console.log(course.nothingToSeeHere);
+  let obj3 = {};
+  obj.doStuff = function() {
+    console.log("Hi, I'm a method!");
+  };
 
-delete course.nothingToSeeHere;
-console.log('Nothing to see: ', course.nothingToSeeHere);
+  console.log('Nothing to see: ', course.nothingToSeeHere);
 
-a = {'foo':1, 'bar':2};
-b = a;
-b['baz'] = 3;
-b.qux = 4;
-console.log(a);      // → { foo: 1, bar: 2, baz: 3, qux: 4 }
+  course.nothingToSeeHere = 'maybe something';
+  console.log(course.nothingToSeeHere);
 
-// true
-console.log(course.hasOwnProperty('name'));
+  delete course.nothingToSeeHere;
+  console.log('Nothing to see: ', course.nothingToSeeHere);
 
-// false
-console.log(course.hasOwnProperty('oh no, not here'));
+  a = {'foo':1, 'bar':2};
+  b = a;
+  b['baz'] = 3;
+  b.qux = 4;
+  console.log(a);      // → { foo: 1, bar: 2, baz: 3, qux: 4 }
 
-console.log('name' in course);          // → true
-console.log('oh no, not here' in course);    // → false
+  // true
+  console.log(course.hasOwnProperty('name'));
 
-for (property in course) {
-  if (course.hasOwnProperty(property)) {
-    console.log(property +  " is " + course[property]);
+  // false
+  console.log(course.hasOwnProperty('oh no, not here'));
+
+  console.log('name' in course);          // → true
+  console.log('oh no, not here' in course);    // → false
+
+  for (property in course) {
+    if (course.hasOwnProperty(property)) {
+      console.log(property +  " is " + course[property]);
+    }
   }
 }
 
+main();
