99d2b2f1fe7134a702faa171a91d9c963ea60a8e
fsande <fsande@ull.es>
2022-03-18 11:16:28 +0000

    Minor fixings

--- src/T1C-Objects-Strings-Arrays/indexof.js
+++ src/T1C-Objects-Strings-Arrays/indexof.js
@@ -8,7 +8,8 @@
   * @since 20.ene.2020
   * @desc indexof example
   *
-  * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this#As_a_DOM_event_handler}
+  * @see {@link
+  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf}
   */
 
 'use strict';
@@ -16,4 +17,3 @@
 console.log([1, 3, 3, 7].indexOf(3));
 console.log([1, 3, 3, 7].indexOf(8));
 console.log([1, 3, 3, 7].indexOf(3, 2));
-
