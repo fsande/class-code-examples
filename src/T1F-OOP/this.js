5fb4011e06ba5d5b9eac3478a7bf5077d5c3b749
fsande <fsande@ull.es>
2022-03-27 17:45:45 +0100

    Minor code fixings

--- src/T1F-OOP/this.js
+++ src/T1F-OOP/this.js
@@ -5,7 +5,7 @@
  * Programación de Aplicaciones Interactivas
  *
  * @author F. de Sande
- * @since 20.feb.2020
+ * @since 2022 March 27
  * @desc The this binding. In methods, this refers to the object that the method was called on
  *       The call() allows for a function/method belonging to one object to be assigned and called for a different object.
  * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/call}
