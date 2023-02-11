88b1b778b4a1604b39951cb7eb63887eecfd65e6
fsande <fsande@ull.es>
2023-02-11 18:20:04 +0000

    constructor overload

---
+++ src/TypeScript/constructor-overloads.ts
@@ -0,0 +1,31 @@
+/**
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * Programación de Aplicaciones Interactivas
+  *
+  * @author F. de Sande
+  * @since Feb 11, 2023
+  * @desc Constructors can be overloaded
+  * @see https://www.typescriptlang.org/docs/handbook/2/classes.html#constructors
+  */
+
+class Point1 {
+  coordX: number;
+  coordY: number;
+
+  // Normal signature with defaults
+  constructor(coordX = 0, coordY = 0) {
+    this.coordX = coordX;
+    this.coordY = coordY;
+  }
+}
+
+class Point2 {
+  // Overloads
+  constructor(coordX: number, coordY: string);
+  constructor(coord: string);
+  constructor(coordX: any, coordY?: any) {
+    // TBD
+  }
+}
---
+++ src/TypeScript/readonly-properties.ts
@@ -0,0 +1,34 @@
+/**
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * Programación de Aplicaciones Interactivas
+  *
+  * @author F. de Sande
+  * @since Feb 11, 2023
+  * @desc readonly properties
+  *       Fields may be prefixed with the readonly modifier. This prevents assignments to the field outside of the constructor.
+  *       if name is readonly this code produces an error
+  */
+
+class Greeter {
+  // name: string = 'world';
+  readonly name: string = 'world';
+ 
+  constructor(otherName?: string) {
+    if (otherName !== undefined) {
+      this.name = otherName;
+    }
+  }
+ 
+  error() {
+    this.name = 'not ok';
+  }
+}
+
+export let main = async () => {
+  const myGreeter = new Greeter();
+  myGreeter.name = 'also not ok';
+};
+
+main();
