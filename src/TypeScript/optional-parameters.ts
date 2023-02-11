8dc9abf8c3c23bf3aa31415cdb1723f075cee77c
fsande <fsande@ull.es>
2023-02-11 16:48:34 +0000

    Code improvement

---
+++ src/TypeScript/optional-parameters.ts
@@ -0,0 +1,31 @@
+/**
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * Programación de Aplicaciones Interactivas
+  *
+  * @author F. de Sande
+  * @since Feb 11, 2023
+  * @desc TS classes. 
+  *       Optional parameters in methods
+  */
+
+class Dog {    
+  age: number;        
+  name: string;    
+  favoriteToy: string;    
+  constructor(age: number, name: string, favoriteToy?: string) {        
+    this.age = age;        
+    this.name = name;        
+    if (favoriteToy !== undefined) {            
+      this.favoriteToy = favoriteToy;        
+    }    
+  }
+}
+
+export let main = async () => {
+  let germanShepherd: Dog = new Dog(7, 'Lucky');
+  let chihuahua: Dog = new Dog(4, 'Osita', 'squeaky bone');
+};
+
+main();
