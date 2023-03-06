1e153a4a2bf0809bbb766020f0e225f5ab6623c2
fsande <fsande@ull.es>
2023-03-06 13:57:42 +0000

    tsconfig updated

---
+++ src/T1C-Objects-Strings-Arrays/prueba.ts
@@ -0,0 +1,27 @@
+/**
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * Programación de Aplicaciones Interactivas
+  *
+  * @author F. de Sande
+  * @since 30.ene.2020
+  * @desc Maps through objects
+  *
+  */
+
+'use strict';
+
+export let main = async () => {
+  let scores = {
+    peach: 100,
+    mario: 88,
+    luigi: 91
+  };
+  scores.toad = 72;
+  let name = 'wario';
+  scores[name] = 102;
+  console.log(scores);   // → 100
+};
+
+main();
