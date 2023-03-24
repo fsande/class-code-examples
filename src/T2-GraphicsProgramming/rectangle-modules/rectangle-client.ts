97ef69f6fa3ef9c964340e15f717a6267aba9465
fsande <fsande@ull.es>
2023-03-24 10:34:32 +0000

    Code improvement

--- src/T2-GraphicsProgramming/rectangle-modules/rectangle-client.ts
+++ src/T2-GraphicsProgramming/rectangle-modules/rectangle-client.ts
@@ -11,12 +11,12 @@
  *              Compile using: tsc --out rectangle.js rectangle-client.ts
  * @see {@link https://stackoverflow.com/questions/71621472/compile-typescript-to-run-in-the-browser-using-modules}
  *              Ha de tenerse en cuenta que:
- *              - El proyecto ha de compilarse usando tsc (sin pasar parámetros, ni el nombre del fichero a compilar) para que tsc utilice la información del tsconfig.json
- *              - El código HTML ha de usar la etiqueta incluyendo type="module" (véase rectangulo.html)
- *              - El código del programa cliente ha de importar el módulo poniendo explícitamente 
+ *              * El proyecto ha de compilarse usando tsc (sin pasar parámetros, ni el nombre del fichero a compilar) para que tsc utilice la información del tsconfig.json
+ *              * El código HTML ha de usar la etiqueta incluyendo type="module" (véase rectangulo.html)
+ *              * El código del programa cliente ha de importar el módulo poniendo explícitamente 
  *                la extensión .js del fichero importado (sentencia import {Rectangle} from './rectangle.js'; )
- *              - En el fichero tsconfig ha de usarse "module": "es2015",
- *              - La página rectangulo.html no funciona si se carga directamente desde un navegador, 
+ *              * En el fichero tsconfig ha de usarse "module": "es2015",
+ *              * La página rectangulo.html no funciona si se carga directamente desde un navegador, 
  *                sino que ha de utilizarse un servidor para alojarla (el que configuraron en su máquina 
  *                virtual o bien usar la extensión live server de VSC).
  */
