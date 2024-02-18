2453caac1289e6997570b01ddb28cd28a53530c2
fsande <fsande@ull.es>
2024-02-18 20:20:14 +0000

    getter without get

--- src/TypeScript/abstract-class.ts
+++ src/TypeScript/abstract-class.ts
@@ -17,11 +17,11 @@
 abstract class Employee {
   constructor(private firstName: string, private lastName: string) { }
   abstract getSalary(): number;
-  get fullName(): string {
+  getfullName(): string {
     return `${this.firstName} ${this.lastName}`;
   }
   compensationStatement(): string {
-    return `${this.fullName} makes ${this.getSalary()} a month.`;
+    return `${this.getfullName()} makes ${this.getSalary()} a month.`;
   }
 }
 
