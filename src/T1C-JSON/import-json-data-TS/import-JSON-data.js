db727f9325b69d1f02cdf393121294e4896667c6
fsande <fsande@ull.es>
2024-03-08 14:21:28 +0000

    Import JSON data in TS

---
+++ src/T1C-JSON/import-json-data-TS/import-JSON-data.js
@@ -0,0 +1,17 @@
+/**
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * Programación de Aplicaciones Interactivas
+  *
+  * @author F. de Sande
+  * @since March 8, 2024
+  * @desc Import data from a local JSON file
+  *       Take into account that you have to use
+  *         "resolveJsonModule": true
+  *       in your tsconfig.json file
+  *
+  * Compile using tsc
+  */
+import capitals from './capitals.json' with { type: 'json' }; // This import style requires "esModuleInterop"
+console.log(capitals);
