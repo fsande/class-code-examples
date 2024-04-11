91bcda66ea153fdf8f05121f2ff99f113e53239b
fsande <fsande@ull.es>
2024-04-11 13:21:17 +0100

    Fix Uncaught ReferenceError: exports is not defined

--- src/T4-Events-TS/25-multiple-present/main.ts
+++ src/T4-Events-TS/25-multiple-present/main.ts
@@ -7,6 +7,16 @@
  * @author F. de Sande
  * @since Apr 23, 2023
  * @description Events. OO Present. Multiple Presents. Main program
+ *              Ha de tenerse en cuenta que:
+ *              * El proyecto ha de compilarse usando tsc sin pasar parámetros para que tsc utilice la información del tsconfig.json
+ *              * El código HTML ha de usar la etiqueta script incluyendo type="module" 
+ *                When using this option (module), the processing of the script contents is deferred.                   
+ *              * El código del programa cliente ha de importar el módulo poniendo explícitamente 
+ *                la extensión .js del fichero importado (sentencia import {App} from './app.js'; )
+ *              * En el fichero tsconfig ha de usarse "module": "es2015",
+ *              * La página html no funciona si se carga directamente desde un navegador, 
+ *                sino que ha de utilizarse un servidor para alojarla (el que configuraron en su máquina 
+ *                virtual o bien usar la extensión live server de VSC).
  */
 
 import {App} from './app.js';
