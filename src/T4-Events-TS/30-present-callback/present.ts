affec4a25348af0ded3be6f04e7ee6adab98f288
fsande <fsande@ull.es>
2024-04-11 13:55:04 +0100

    Code improvement

--- src/T4-Events-TS/30-present-callback/present.ts
+++ src/T4-Events-TS/30-present-callback/present.ts
@@ -6,28 +6,24 @@
  *
  * @author F. de Sande
  * @since Apr 23, 2023
- * @description OO Present application with callback. Present class
+ * @description OO Multiple Present application
+ *              Communication between classes is implemented using a callback
  */
 
 /** 
  * @class
- * @description Present Class. Represents a Present.
- *       Initially the present has an image that will be replaced
- *       by the present image when the original is clicked
+ * @description Represents a Present.
+ *              Initially the present has an image that will be replaced
+ *              by the present image when the original is clicked
  */
 export class Present {
-  private containerElement: HTMLElement;   /** DOM element that holds the presents */
-  private presentSrc: string;         /** URL of the present new image */
-  private onOpenCallback: () => void;     /** Callback function to be called when present opens */ 
-  private image: HTMLImageElement;              /** New DOM element to hold the present image */
-
   /*
    * @constructor
    * @param {object} containerElement - DOM element that holds the present image
    * @param {string} presentSrc - URL of the image to show when the present is opened
    * @param {function} onOpenCallback - Callback to be invoked when the present is opened
    */
-  constructor(containerElement: HTMLElement, presentSrc: string, onOpenCallback: () => void) {
+  constructor(private containerElement: HTMLElement, private presentSrc: string, private onOpenCallback: () => void, private image?: HTMLImageElement) {
     this.containerElement = containerElement;
     this.presentSrc = presentSrc;
     this.onOpenCallback = onOpenCallback;
@@ -45,8 +41,8 @@ export class Present {
    * @param {object} event - Event object
    */
   private openPresent = (event: Event): void => {
-    this.image.src = this.presentSrc;
-    this.image.removeEventListener('click', this.openPresent);
+    this.image!.src = this.presentSrc;
+    this.image!.removeEventListener('click', this.openPresent);
     this.onOpenCallback();
   }
 }
